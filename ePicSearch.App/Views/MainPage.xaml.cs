c5855351bab774a644d2eab6c3e286e230813027
MikeTat1988 <michaeltatev@gmail.com>
2025-01-14 16:46:12 +0200

    removed the tutorial button

--- ePicSearch.App/Views/MainPage.xaml.cs
+++ ePicSearch.App/Views/MainPage.xaml.cs
@@ -43,7 +43,7 @@ namespace ePicSearch.Views
                 var blurBehavior = BackgroundImage.Behaviors.OfType<BlurBehavior>().FirstOrDefault();
                 var blurTask = blurBehavior?.AnimateBlurEffect(10, 100);
                 var fadeTask = BackgroundImage.FadeTo(0.6, 2000, Easing.CubicInOut);
-                var buttonsAppearTask = ButtonsAppear(MyAdventuresButton, CreateAdventureButton, SettingsButton, QuitButton, TutorialButton, TitleImage);
+                var buttonsAppearTask = ButtonsAppear(MyAdventuresButton, CreateAdventureButton, SettingsButton, QuitButton, TitleImage);
 
                 await Task.WhenAll(zoomTask, blurTask!, fadeTask, buttonsAppearTask);
 
@@ -82,13 +82,6 @@ namespace ePicSearch.Views
             ClickButton(sender);
 
             await Navigation.PushAsync(new MyAdventuresPage(_adventureManager, _logger, _audioPlayerService));
-        }
-        
-        private async void OnTutorialClicked(object sender, EventArgs e)
-        {
-            ClickButton(sender);
-
-            await Navigation.PushAsync(new TutorialPage(_logger, _audioPlayerService));
         }
 
         private async void OnCreateNewAdventureClicked(object sender, EventArgs e)
